ae39ff19cc32cfc6d4f88b632e4dd16ff1b48f4c
James Lang <james.lang@okstate.edu>
2025-11-19 23:26:11 -0500

    Added predictions link

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,15 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import Link from 'next/link';
 export default function Navbar() {
   return (
             <nav className="navbar-bg">
                 <div className="flex items-center">
                     <Image src="/logoandtext.png" alt="Heater Logo" width={200} height={70} />
+                </div>
+                <div className="flex items-center space-x-4">
+                  <Link href="/picks" className="text-white hover:text-[#ff9f1c]">
+                    Make Picks
+                  </Link>
                 </div>
             </nav>
   )
